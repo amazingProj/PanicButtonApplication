073ff09891d49d3c86d491139aa4e2acd2654af6
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2021-10-10 11:43:47 +0300

    Button UI with documentation

--- app/src/main/java/com/example/custombutton/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/custombutton/main/activity/MainActivity.kt
@@ -1,27 +1,31 @@
-package com.example.custombutton.ui
+package com.example.custombutton.main.activity
 
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.media.AudioManager
 import android.media.MediaPlayer
-import android.media.SoundPool
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.custombutton.R
 
+
+/**
+ * class represents the main activity includes emergency button
+ */
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
     private var media: MediaPlayer? = null
-    private val soundId = 1
+
+    /**
+     * creates ui of the screen which in activity_main
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.button)
-        media = MediaPlayer.create(this, R.raw.emergency_sound)
     }
 
     fun playSound(view: View) {
+        media = MediaPlayer.create(this, R.raw.emergency_sound)
         media?.start()
        Toast.makeText(applicationContext,"הפעלת את אזעקת המצוקה" ,Toast.LENGTH_SHORT).show()
     }
---
+++ app/src/main/java/com/example/custombutton/main/model/Observer.kt
@@ -0,0 +1,10 @@
+package com.example.custombutton.main.model
+
+/**
+ * represents observer
+ * when the user click the button we use observer pattern
+ * to update all related function in application
+ */
+interface Observer {
+
+}
